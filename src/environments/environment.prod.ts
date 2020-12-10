7febce973e92c8504b507f924403b13107b77269
Andrei Mihai <andrei.mihai@softvision.com>
2020-12-10 11:54:06 +0200

    add first 3dcss example

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBWjMa8adLpGBRWh66bw8lkDTbHbz_8wUo",
+    authDomain: "babylon-js-with-angular.firebaseapp.com",
+    databaseURL: "https://babylon-js-with-angular.firebaseio.com",
+    projectId: "babylon-js-with-angular",
+    storageBucket: "babylon-js-with-angular.appspot.com",
+    messagingSenderId: "824212666012",
+    appId: "1:824212666012:web:7ee56d36af36630635df59",
+  },
+};
