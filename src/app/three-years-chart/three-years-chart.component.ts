e14be50b31dc14066e5a5fae9288f74f742789b3
Andrei Mihai <andrei.mihai@softvision.com>
2021-01-18 16:21:06 +0200

    add tooltip responsive position

--- src/app/three-years-chart/three-years-chart.component.ts
+++ src/app/three-years-chart/three-years-chart.component.ts
@@ -13,15 +13,24 @@ export class ThreeYearsChartComponent implements OnInit {
 
   displayModel: ThreeYearsChartDisplayModel[] = [];
 
-  constructor(private elementRef: ElementRef, private window: Window) {
-  }
+  constructor(private elementRef: ElementRef, private window: Window) { }
 
   @HostListener('mouseover') onMouseOver(): void {
+    const nativeElement = this.elementRef.nativeElement as HTMLElement;
+
     this.columns.forEach((column: ElementRef, index: number) => {
-      const height = this.getCSSStyle(column.nativeElement).height;
+      const height = this.getAfterCSSStyle(column.nativeElement).height;
 
       if (height !== 'auto') {
-        console.log(index, height);
+        let tooltipBottom = 0;
+
+        if (this.displayModel[index].bottom + parseInt(height, 10) > nativeElement.offsetHeight * 0.9) {
+          tooltipBottom = this.displayModel[index].bottom - 2 * parseInt(height, 10);
+        } else {
+          tooltipBottom = this.displayModel[index].bottom + parseInt(height, 10);
+        }
+
+        column.nativeElement.style.setProperty('--tooltipBottom', `${tooltipBottom}`);
       }
     }
     );
@@ -33,8 +42,9 @@ export class ThreeYearsChartComponent implements OnInit {
 
     this.displayModel = this.generateDisplayModel(this.input, nativeElement.offsetHeight, nativeElement.offsetWidth);
 
-    nativeElement.style.setProperty('--ratio', `${ratio}`);
+    nativeElement.style.setProperty('--middleOfColumnWidth', `${nativeElement.offsetWidth / this.input.length / 2}px`);
     nativeElement.style.setProperty('--height', `${nativeElement.offsetHeight}`);
+    nativeElement.style.setProperty('--ratio', `${ratio}`);
     nativeElement.style.setProperty('--width', `${nativeElement.offsetWidth}`);
   }
 
@@ -42,7 +52,7 @@ export class ThreeYearsChartComponent implements OnInit {
     this.onResize();
   }
 
-  getCSSStyle(element: Element): CSSStyleDeclaration {
+  getAfterCSSStyle(element: Element): CSSStyleDeclaration {
     return this.window.getComputedStyle(element, ':after');
   }
 
@@ -64,12 +74,12 @@ export class ThreeYearsChartComponent implements OnInit {
   private generateDisplayModel(values: ThreeYearsChartInput[], widgetHeight: number, widgetWidth: number): ThreeYearsChartDisplayModel[] {
     const base = widgetWidth / values.length;
     const displayModel: ThreeYearsChartDisplayModel[] = [];
-    const leftOffset = base / 2;
     const mapValues = (difference: number = 0) => values.map(value => value.value - difference);
     const valueArray = values.map(entry => entry.value);
     const bottomMostPoint = Math.min(...valueArray);
     const topMostPoint = Math.max(...valueArray);
     let fixedNegativeValues = [];
+    let leftOffset = base / 2;
     let padding = widgetHeight * 0.05;
     let chartHeight = widgetHeight * 0.9 - padding;
     let rise = 0;
@@ -83,8 +93,10 @@ export class ThreeYearsChartComponent implements OnInit {
     }
 
     for (let i = 0; i < values.length - 1; i++) {
-      const currentValue = new ThreeYearsChartDisplayModel(0, 0, 0, leftOffset, values[i].value, values[i].year);
+      const currentValue = new ThreeYearsChartDisplayModel(0, 0, 0, 0, values[i].value, values[i].year);
       currentValue.bottom = chartHeight * fixedNegativeValues[i] / topMostPoint + padding;
+      currentValue.left = leftOffset;
+      leftOffset += base;
       rise = currentValue.bottom - chartHeight * fixedNegativeValues[i + 1] / topMostPoint - padding;
       currentValue.hypotenuse = Math.sqrt(base * base + rise * rise);
       currentValue.angle = Math.asin(rise / currentValue.hypotenuse) * 180 / Math.PI;
